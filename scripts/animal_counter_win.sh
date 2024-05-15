69201279478a1d2a225954bf39ad69daf7f9e181
Dave Wheeler <dwheelerau@gmail.com>
2024-05-15 12:09:32 +1000

    Update animal_counter_win.sh

--- scripts/animal_counter_win.sh
+++ scripts/animal_counter_win.sh
@@ -21,6 +21,7 @@ echo "target FPS used is: " $target_fps
 python scripts/get_metadata.py $vid $target_fps
 # per = FPS of video / desired FPS
 per_param=$(cat config/rate_file.txt)
+echo "Using per value of: " $per_param
 video-toimg --per $per_param $vid
 
 # label
