8ce832b7c0c9ef31f30617aa2c17f5df6ea03cd8
Dave Wheeler <dwheelerau@gmail.com>
2024-05-15 11:45:07 +1000

    Update get_metadata.py

--- scripts/get_metadata.py
+++ scripts/get_metadata.py
@@ -2,12 +2,15 @@ import imageio
 import sys
 import time
 
-video_file = sys.argv[1]
+video_file = sys.argv[1] 
 reader = imageio.get_reader(video_file)
 fps=reader.get_meta_data()['fps']
 
 # target fps 
-target_fps=1
+target_fps=sys.argv[2]
+if len(target_fps) < 1:
+    target_fps = 1
+target_fps = float(target_fps) 
 per=str(int(target_fps*fps))
 
 with open('config/rate_file.txt', 'w') as wf:
